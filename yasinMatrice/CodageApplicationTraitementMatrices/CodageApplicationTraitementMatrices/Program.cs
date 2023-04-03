e6ce9b688e0a4849f38c37015ab54890f1f4baaf
yasin <115707844+Yasingkv@users.noreply.github.com>
2023-04-03 22:46:55 +0200

    Add files via upload

---
+++ yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace CodageApplicationTraitementMatrices
+{
+    public struct outils
+    {
+
+        public void LireReel(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention ! vous devez taper un nombre réel !");
+                nUser = Console.ReadLine();
+            }
+        }
+
+        public void affichage(int[] tableau, out string aff)
+        {
+            aff = "";
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                aff = aff + tableau[i] + "|";
+            }
+        }
+
+        public void creationAleatoire(int nombre, int max, int min, out int[] tableau)
+        {
+            tableau = new int[nombre];
+            Random alea = new Random();
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                tableau[i] = alea.Next(min, max + 1);
+            }
+        }
+        public void Generation_Tableau(out int[,] grille)
+        {
+            int ligne = 0;
+            int colonne = 0;
+            grille = new int[ligne, colonne];
+            for (ligne = 0; ligne < grille.GetLength(0)-1; ligne++)
+            {
+                for (colonne = 0; colonne < grille.GetLength(1)-1; colonne++)
+                {
+                    grille[ligne, colonne] = alea(0, 20);
+                }
+            }
+        }
+
+        public void String_Tableau(int[,] grille, out string stringTab)
+        {
+            stringTab = "";
+            for (int ligne = 0; ligne < grille.GetLength(0)-1; ligne++)
+            {
+                for (int colonne = 0; colonne < grille.GetLength(1)-1; colonne++)
+                {
+                    stringTab += grille[ligne, colonne] + "|";
+                }
+            }
+            stringTab[ligne, colonne] = "\n";
+        }
+
+        public void Addition_Matrices(int[,] tab1, int[,] tab2, out int[,] tab3, out bool addition)
+        {
+            int ligne;
+            int colonne;
+            tab1 = new int[ligne, colonne];
+            tab2 = new int[ligne, colonne];
+            addition = true;
+            if (tab1.GetLength(0) == tab2.GetLength(0) && tab1.GetLength(1) == tab2.GetLength(1))
+            {
+                for (int ligne = 0; ligne < tab1.GetLength(0)-1; ligne++)
+                {
+                    for (int colonne = 0; colonne < tab1.GetLength(1)-1; colonne++)
+                    {
+                        tab3[ligne, colonne] += tab1[ligne, colonne] + tab2[ligne, colonne];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/outil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodageApplicationTraitementMatrices
+{
+    public void Main(string[] args)
+    {
+        outils mesOutils = new outils();
+        int choix;
+        int ligne = 0;
+        int colonne = 0;
+        int[,] grille = new int[ligne,colonne];
+        int[] tableau;
+        string aff;
+        string stringTab;
+        int[,] tab1;
+        int[,] tab2;
+        int[,] tab3;
+        bool addition;
+        int nombre;
+        int min;
+        int max;
+        do
+        {
+            mesOutils.LireReel("quel matrices choississez-vous ? \n   1 = Generation_Tableau\n   2 = String_Tableau\n   3 =Addition_Matrices \n", out choix);
+            mesOutils.LireReel("dimension de la matrice", out nombre);
+            mesOutils.LireReel("min", out min);
+            mesOutils.LireReel("max", out max);
+            mesOutils.creationAleatoire(nombre, min, max, out tableau);
+
+            if (choix == 1)
+            {
+                mesOutils.Generation_Tableau(out grille);
+                mesOutils.affichage(tableau, out aff);
+            }
+            if (choix == 2)
+            {
+                mesOutils.String_Tableau(grille, out stringTab);
+                mesOutils.affichage(tableau, out aff);
+            }
+            if (choix == 3)
+            {
+                mesOutils.Addition_Matrices(tab1, tab2, out tab3, out addition);
+                mesOutils.affichage(tableau, out aff);
+            }
+        } while (true);
+        
+    }
+}
