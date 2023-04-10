991e8d94d24adcff6145aac58823603ea6f07708
Yasingkv <200460@site.asty-moulin.be>
2023-04-10 23:12:40 +0200

    fin matrice

--- yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/Program.cs
+++ yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/Program.cs
@@ -16,27 +16,63 @@ namespace CodageApplicationTraitementMatrices
             string stringTab;
             int nbligne;
             int nbcolonne;
+            int[,] t1;
+            int[,] t2;
+            int[,] t3;
+            bool addition;
+            int[,] matriceMulti;
+            bool test;
             do
             {
-                mesOutils.LireReel("quel matrices choississez-vous ? \n   1 = generation_tableau\n   2 = concatenation \n   3 =Addition_Matrices \n  4 = multiplication \n", out choix);
+                mesOutils.LireReel("quel matrices choississez-vous ? \n   1 = generation_tableau\n   2 = Addition_Matrices \n   3 = multiplication \n", out choix);
 
                 if (choix == 1)
                 {
-                    mesOutils.LireReel("ligne", out nbligne);
-                    mesOutils.LireReel("colonne", out nbcolonne);
+                    mesOutils.LireReel("nombre de ligne : ", out nbligne);
+                    mesOutils.LireReel("nombre de colonne : ", out nbcolonne);
                     mesOutils.Generation_Tableau(out grille, nbligne, nbcolonne );
                     mesOutils.String_Tableau( grille, out stringTab);
                     Console.WriteLine(stringTab);
                 }
                 if (choix == 2)
                 {
-                    
+                    mesOutils.LireReel("nombre de ligne : ", out nbligne);
+                    mesOutils.LireReel("nombre de colonne : ", out nbcolonne);
+                    mesOutils.Generation_Tableau(out t1, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(t1, out stringTab);
+                    Console.WriteLine(stringTab);
+
+                    mesOutils.LireReel("nombre de ligne : ", out nbligne);
+                    mesOutils.LireReel("nombre de colonne : ", out nbcolonne);
+                    mesOutils.Generation_Tableau(out t2, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(t2, out stringTab);
+                    Console.WriteLine(stringTab);
+
+                    mesOutils.Addition_Matrices(t1, t2, out t3, out addition, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(t3, out stringTab);
+                    Console.WriteLine(stringTab);
                 }
                 if (choix == 3)
                 {
-                    
+                    mesOutils.LireReel("nombre de ligne : ", out nbligne);
+                    mesOutils.LireReel("nombre de colonne : ", out nbcolonne);
+                    mesOutils.Generation_Tableau(out t1, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(t1, out stringTab);
+                    Console.WriteLine(stringTab);
+
+                    mesOutils.LireReel("nombre de ligne : ", out nbligne);
+                    mesOutils.LireReel("nombre de colonne : ", out nbcolonne);
+                    mesOutils.Generation_Tableau(out t2, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(t2, out stringTab);
+                    Console.WriteLine(stringTab);
+
+
+                    mesOutils.multiplicationMatrice(t1, t2, out matriceMulti, out test, nbligne, nbcolonne);
+                    mesOutils.String_Tableau(matriceMulti, out stringTab);
+                    Console.WriteLine(stringTab);
                 }
             } while (true);
+
 
         }
     }
--- yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/outils.cs
+++ yasinMatrice/CodageApplicationTraitementMatrices/CodageApplicationTraitementMatrices/outils.cs
@@ -27,7 +27,7 @@ namespace CodageApplicationTraitementMatrices
             {
                 for (int colonne = 0; colonne <= nbcolonne - 1; colonne++)
                 {
-                    grille[ligne, colonne] = alea.Next(0, 20);
+                    grille[ligne, colonne] = alea.Next(0, 10);
                 }
             }
         }
@@ -36,18 +36,54 @@ namespace CodageApplicationTraitementMatrices
         {
             
             stringTab = "";
-            for (int ligne = 0; ligne < grille.GetLength(0)-1; ligne++)
+            for (int ligne = 0; ligne < grille.GetLength(0); ligne++)
             {
-                for (int colonne = 0; colonne < grille.GetLength(1)-1; colonne++)
+                for (int colonne = 0; colonne < grille.GetLength(1); colonne++)
                 {
-                    stringTab += grille[ligne, colonne] + "|";
+                    stringTab += grille[ligne, colonne] + "  |  ";
                 }
                 stringTab += "\n";
             }
 
             
         }
+        public void Addition_Matrices(int[,] t1, int[,] t2, out int[,] t3, out bool addition, int nbLigne, int nbColonne)
+        {
+            t3 = new int[nbLigne, nbColonne];
+            addition = true;
+            if (t1.GetLength(0) == t2.GetLength(0) && t1.GetLength(1) == t2.GetLength(1))
+            {
+                for (nbLigne = 0; nbLigne < t1.GetLength(0) - 1; nbLigne++)
+                {
+                    for (nbColonne = 0; nbColonne < t1.GetLength(1) - 1; nbColonne++)
+                    {
+                        t3[nbLigne, nbColonne] += t1[nbLigne, nbColonne] + t2[nbLigne, nbColonne];
+                    }
+                }
+            }
+        }
 
-        
+        public void multiplicationMatrice(int[,] t1, int[,] t2, out int[,] matriceMulti, out bool test, int nbLigne, int nbColonne)
+        {
+            matriceMulti = new int[nbLigne, nbColonne];
+            test = true;
+            if (t1.GetLength(1) == t2.GetLength(0))
+            {
+                for (int ligne = 0; ligne < t1.GetLength(0) - 1; ligne++)
+                {
+                    for (int colonne = 0; colonne < t1.GetLength(1) - 1; colonne++)
+                    {
+                        matriceMulti[ligne, colonne] = 0;
+                        for (int i = 0; i < t1.GetLength(1) - 1; i++)
+                        {
+                            matriceMulti[ligne, colonne] += t1[ligne, i] * t2[i, colonne];
+                        }
+                    }
+                }
+                test = false;
+            }
+        }
+
+
     }
 }
